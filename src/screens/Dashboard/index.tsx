24ff92747cb1be045064cfd1c98ba2a106d38b48
Thiago Miziara <thiagonmiziara@gmail.com>
2021-10-30 13:11:45 -0300

    add styles header user

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 import * as S from "./styles";
 
 export function Dashboard() {
   return (
     <S.Container>
-      <S.Title>Dashboard</S.Title>
+      <S.Header>
+        <S.UserWrapper>
+          <S.UserInfo>
+            <S.UserPhoto
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/67029929?v=4",
+              }}
+            />
+            <S.User>
+              <S.UserGreeting>Olá,</S.UserGreeting>
+              <S.UserName>Thiago</S.UserName>
+            </S.User>
+          </S.UserInfo>
+        </S.UserWrapper>
+      </S.Header>
     </S.Container>
   );
 }
